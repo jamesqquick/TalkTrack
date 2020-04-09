9024f29580748d69f88b12484b0ddb7192e510e0
James Quick <james.q.quick@outlook.com>
2020-04-09 13:32:09 -0500

    Check is browser before navigating

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -6,7 +6,9 @@ export default function ProtectedRoute({ children }) {
   const { loading, isAuthenticated } = useAuth0();
 
   if (loading) return <p>Loading...</p>;
-  if (!isAuthenticated) {
+  const isBrowser = typeof window !== 'undefined';
+
+  if (!isAuthenticated && isBrowser) {
     navigate('/');
   }
   return { ...children };
